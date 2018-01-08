48bfd6926dcce3b738fcd676178604fe3a04eda0
edjuaro <edjuaro@gmail.com>
2018-01-08 10:38:47 -0800

    adding a notebook with instructions to download manifest and support images

--- step_1/download_from_manifest.py
+++ step_1/download_from_manifest.py
@@ -1,59 +1,33 @@
-import subprocess
-from subprocess import call
-import pandas as pd
-import os
-import shutil
-import gzip
-import json
-import sys
+from dfm_functions import *
+import argparse
 
-
-def uncompress_gzip(file_name, new_name=None, delete=True):
-    # Read the stream and write that stream to a new file:
-    in_file = gzip.open(file_name, 'rb')
-    if new_name is None:
-        out_file = open(file_name.strip('.gz'), 'wb')
-    else:
-        out_file = open(new_name, 'wb')
-    out_file.write(in_file.read())
-    in_file.close()
-    out_file.close()
-    if delete:
-        os.remove(file_name)
-
-
-def execute(comando, doitlive=False, input_to_use=None):
-    # result = subprocess.run(['ls', '-l'], stdout=subprocess.PIPE)
-    comando = comando.split(' ')
-
-    if doitlive:
-        popen = subprocess.Popen(comando, stdout=subprocess.PIPE, universal_newlines=True)
-        to_return = popen.stdout.read()
-        for line in to_return:
-            print(line, end='')
-        popen.stdout.close()
-        return_code = popen.wait()
-        if return_code:
-            raise subprocess.CalledProcessError(return_code, comando)
-    else:
-        if input_to_use is not None:
-            input_to_use = input_to_use.ecode('utf-8')
-        result = subprocess.run(comando, stdout=subprocess.PIPE, stderr=subprocess.PIPE, input=input_to_use)
-        to_return = result.stdout.decode('utf-8')
-        print(to_return)
-    return to_return.strip('\n')
+parser = argparse.ArgumentParser()
+# ~~~~Arguments~~~~~ #
+parser.add_argument("-m", "--manifest", type=str,
+                    help="The relative path of the manifest used to download the data")
+parser.add_argument("-n", "--metadata", type=str,
+                    help="The relative path of the metadata file")
+parser.add_argument("-v", "--verbose", action="store_true",
+                    help="increase output verbosity")
+args = parser.parse_args()
+if args.verbose:
+    print("We like being verbose!")
 
 
 # Download from manifest
-manifest = "gdc_manifest_20171221_005438.txt"
+# manifest = "gdc_manifest_20171221_005438.txt"
+manifest = args.manifest
+# metadata_file = "metadata.cart.2017-12-21T21_41_22.870798.json"
+metadata_file = args.metadata
+
 command = "./gdc-client download -m "+manifest
+print("About to execute the command:", command)
 what = execute(command)
 print('All files in manifest were downloaded, probably. To be sure, check the output of the gdc-client.')
 
 dfest = pd.read_table(manifest)
 
 # Parse metadata
-metadata_file = "metadata.cart.2017-12-21T21_41_22.870798.json"
 meta_data = json.load(open(metadata_file))
 
 # Make a dictionary to map file names to TCGA unique ID's
